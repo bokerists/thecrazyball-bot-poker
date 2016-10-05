274614459a511aa468d6d6c76c579eb479f11aaf
Davide Cantelli <cant1989@gmail.com>
2016-10-05 18:54:45 +0200

    dass

--- player.js
+++ player.js
@@ -9,15 +9,6 @@ exports = module.exports = {
     // gamestate contains info about the state of the game.
     // check the documentation at https://bot-poker.herokuapp.com/wiki#gamestate for further info about the data structure.
 
-    //
-    // you just have to return the amount that you want to bet.
-
-
-
-    // enjoy the game!
-
-    //
-    // currently we just fold every single hand.
 
     'use strict';
 
